f451379cbc739718cbc11caa696cf65d4bee6129
Timovzl <655426+Timovzl@users.noreply.github.com>
2023-05-09 17:53:57 +0200

    WrapperConverter: Simplified and improved performance.

--- ToDoBoundedContextName/Infrastructure.Databases/WrapperConverter.cs
+++ ToDoBoundedContextName/Infrastructure.Databases/WrapperConverter.cs
@@ -9,19 +9,23 @@ namespace __ToDoAreaName__.__ToDoBoundedContextName__.Infrastructure.Databases;
 
 /// <summary>
 /// <para>
-/// Converts between types <typeparamref name="TModel"/> and <typeparamref name="TProvider"/>, where <typeparamref name="TModel"/> is a type containing a single (non-public) instance field of type <typeparamref name="TProvider"/>.
+/// Converts between types <typeparamref name="TModel"/> and <typeparamref name="TProvider"/> without using constructors, where <typeparamref name="TModel"/> is a type containing a single (non-public) instance field of type <typeparamref name="TProvider"/>.
 /// </para>
 /// <para>
-/// This type primarily supports wrapper value objects.
+/// This type primarily is primarily intended for converting between wrapper value objects and primitives.
 /// </para>
 /// <para>
-/// The advantage of this type over <see cref="CastingConverter{TModel, TProvider}"/> is that this avoids constructors.
-/// For example, if a domain rule changes but existing data is not changed, the constructor would refuse to load existing data from the database, which is undesirable.
+/// Unlike <see cref="CastingConverter{TModel, TProvider}"/>, the current converter actively avoids constructors.
+/// For example, if a domain rule in <typeparamref name="TModel"/>'s constructor changes but existing data is allowed to remain unchanged, the constructor might refuse to load such data from the database.
 /// </para>
 /// </summary>
 internal sealed class WrapperConverter<TModel, TProvider> : ValueConverter<TModel, TProvider>
 {
 	private static readonly MethodInfo GetUninitializedObjectMethod = typeof(FormatterServices).GetMethod(nameof(FormatterServices.GetUninitializedObject))!;
+
+	private static readonly Lazy<Func<object, TProvider, object>> FieldSetter = new Lazy<Func<object, TProvider, object>>(
+		() => CreateFieldSetter(GetExpectedField()),
+		LazyThreadSafetyMode.ExecutionAndPublication);
 
 	public WrapperConverter()
 		: base(CreateConversionToProviderExpression(), CreateConversionToModelExpression())
@@ -48,12 +52,9 @@ internal sealed class WrapperConverter<TModel, TProvider> : ValueConverter<TMode
 
 	private static Expression<Func<TProvider, TModel>> CreateConversionToModelExpression()
 	{
-		var field = GetExpectedField();
-		var fieldSetter = FieldSetterCache.GetFieldSetter(field);
-
 		var param = Expression.Parameter(typeof(TProvider), "value");
 		var instance = Expression.Call(GetUninitializedObjectMethod, arguments: Expression.Constant(typeof(TModel)));
-		var assignmentAndInstanceResult = Expression.Invoke(Expression.Constant(fieldSetter), instance, param);
+		var assignmentAndInstanceResult = Expression.Invoke(Expression.Constant(FieldSetter.Value), instance, param);
 		var typedInstanceResult = Expression.Convert(assignmentAndInstanceResult, typeof(TModel));
 		var conversion = Expression.Lambda<Func<TProvider, TModel>>(typedInstanceResult, param);
 
@@ -61,54 +62,40 @@ internal sealed class WrapperConverter<TModel, TProvider> : ValueConverter<TMode
 	}
 
 	/// <summary>
-	/// Compiles and caches field setter delegates for fields for the current combination of <typeparamref name="TModel"/> and <typeparamref name="TProvider"/>.
+	/// Creates a new function that can write to a field.
+	/// The result works as expected, even for readonly fields and structs.
 	/// </summary>
-	private static class FieldSetterCache
+	private static Func<object, TProvider, object> CreateFieldSetter(FieldInfo field)
 	{
-		private static readonly ConcurrentDictionary<FieldInfo, Func<object, TProvider, object>> SettersByField = new ConcurrentDictionary<FieldInfo, Func<object, TProvider, object>>(concurrencyLevel: 1, capacity: 5);
+		// We must write IL to achieve the following:
+		// - Write to readonly fields
+		// - Mutate structs in-place (instead of the normal semantics of mutating a copy, which would not help us)
 
-		public static Func<object, TProvider, object> GetFieldSetter(FieldInfo field)
+		var setter = new DynamicMethod(
+			name: $"SetFieldValue_{field.DeclaringType?.Name}_{field.Name}",
+			returnType: typeof(object),
+			parameterTypes: new[] { typeof(object), typeof(TProvider), },
+			m: typeof(WrapperConverter<TModel, TProvider>).Module,
+			skipVisibility: true);
+
+		var ilGenerator = setter.GetILGenerator();
+
+		ilGenerator.Emit(OpCodes.Ldarg_0); // Load the instance to write to, which will be our return value
+		ilGenerator.Emit(OpCodes.Ldarg_0); // Load the instance to write to
+
+		// For value-type instances
+		if (field.DeclaringType?.IsValueType == true)
 		{
-			var result = SettersByField.GetOrAdd(field, CreateFieldSetter);
-			return result;
+			ilGenerator.DeclareLocal(field.DeclaringType.MakeByRefType()); // Create a local by-ref var
+			ilGenerator.Emit(OpCodes.Unbox, field.DeclaringType); // Unbox the object
+			ilGenerator.Emit(OpCodes.Stloc_0); // Assign the result of the unboxing to the by-ref var
+			ilGenerator.Emit(OpCodes.Ldloc_0); // Load the by-ref var for us to write to
 		}
 
-		/// <summary>
-		/// Creates a new function that can write to a field.
-		/// The result works as expected, even for readonly fields and structs.
-		/// </summary>
-		private static Func<object, TProvider, object> CreateFieldSetter(FieldInfo field)
-		{
-			// We must write IL to achieve the following:
-			// - Write to readonly fields
-			// - Mutate structs in-place (instead of the normal semantics of mutating a copy, which would not help us)
+		ilGenerator.Emit(OpCodes.Ldarg_1); // Load the value to be assigned
+		ilGenerator.Emit(OpCodes.Stfld, field); // Assign the new value to the instance's field
+		ilGenerator.Emit(OpCodes.Ret); // Return
 
-			var setter = new DynamicMethod(
-				name: $"AssignFieldValue_{field.Name}",
-				returnType: typeof(object),
-				parameterTypes: new[] { typeof(object), typeof(TProvider), },
-				m: typeof(FieldSetterCache).Module,
-				skipVisibility: true);
-
-			var ilGenerator = setter.GetILGenerator();
-
-			ilGenerator.Emit(OpCodes.Ldarg_0); // Load the instance to write to, which will be our return value
-			ilGenerator.Emit(OpCodes.Ldarg_0); // Load the instance to write to
-
-			// For value-type instances
-			if (field.DeclaringType?.IsValueType == true)
-			{
-				ilGenerator.DeclareLocal(field.DeclaringType.MakeByRefType()); // Create a local by-ref var
-				ilGenerator.Emit(OpCodes.Unbox, field.DeclaringType); // Unbox the object
-				ilGenerator.Emit(OpCodes.Stloc_0); // Assign the result of the unboxing to the by-ref var
-				ilGenerator.Emit(OpCodes.Ldloc_0); // Load the by-ref var for us to write to
-			}
-
-			ilGenerator.Emit(OpCodes.Ldarg_1); // Load the value to be assigned
-			ilGenerator.Emit(OpCodes.Stfld, field); // Assign the new value to the instance's field
-			ilGenerator.Emit(OpCodes.Ret); // Return
-
-			return setter.CreateDelegate<Func<object, TProvider, object>>();
-		}
+		return setter.CreateDelegate<Func<object, TProvider, object>>();
 	}
 }
