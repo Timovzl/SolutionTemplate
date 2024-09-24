e497c8fe72882ce30ef3d6eca2d0992fc317df7f
Timovzl <655426+Timovzl@users.noreply.github.com>
2024-09-24 10:46:00 +0200

    Made MetricLogEventSink pre-init to 0 for each label

--- ToDoBoundedContextName/Application/Logging/MetricLogEventSink.cs
+++ ToDoBoundedContextName/Application/Logging/MetricLogEventSink.cs
@@ -13,9 +13,20 @@ internal sealed class MetricLogEventSink : ILogEventSink
 {
 	private static readonly Counter LogCounter = Metrics.CreateCounter(name: "log_count", help: "Counts log entries per severity.", labelNames: "severity");
 
+	static MetricLogEventSink()
+	{
+		// For each label, initialize the count to 0 to have the metric present, which helps prevent the first increase from being ignored
+		LogCounter.WithLabels([nameof(LogLevel.Trace)]).Inc(0);
+		LogCounter.WithLabels([nameof(LogLevel.Debug)]).Inc(0);
+		LogCounter.WithLabels([nameof(LogLevel.Information)]).Inc(0);
+		LogCounter.WithLabels([nameof(LogLevel.Warning)]).Inc(0);
+		LogCounter.WithLabels([nameof(LogLevel.Error)]).Inc(0);
+		LogCounter.WithLabels([nameof(LogLevel.Critical)]).Inc(0);
+	}
+
 	public void Emit(LogEvent logEvent)
 	{
-		LogCounter.WithLabels(logEvent.Level.ToString()).Inc();
+		LogCounter.WithLabels([logEvent.Level.ToString()]).Inc();
 	}
 }
 
